49d16593be69f8a35220a701dd194344a6eadee4
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-02-27 21:08:43 +0000

    feat: fix neotree and add lsp support to it

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -7,8 +7,27 @@ return {
       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
       'MunifTanjim/nui.nvim',
     },
+    config = function()
+      require('neo-tree').setup {
+        close_if_last_window = false,
+        enable_git_status = true,
+        window = {
+          position = 'right',
+        },
+      }
+    end,
     keys = {
-      vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle right<CR>'),
+      vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>'),
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
   },
 }
