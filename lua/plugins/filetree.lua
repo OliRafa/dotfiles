8c6a5bfb50019f470df3ebec3c5af3756f292218
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-02-27 20:00:22 +0000

    feat: add filetree

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,4 +1,14 @@
 return {
-  { 'preservim/nerdtree' },
-  { 'ryanoasis/vim-devicons' },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    keys = {
+      vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle right<CR>'),
+    },
+  },
 }
