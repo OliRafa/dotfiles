38c4fcbebfee9f6955c849e448400686524a8f5d
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-02-28 17:26:18 +0000

    refac(lazygit): change keymap to leader+g

--- lua/plugins/git-tools.lua
+++ lua/plugins/git-tools.lua
@@ -30,7 +30,7 @@ return {
       'nvim-lua/plenary.nvim',
     },
     keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
 }
