ed610c9e492f3345ccb28f0bc60c40a3358821b7
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-02-28 11:08:55 +0000

    feat: add bracket pairs plugin

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -114,16 +114,11 @@ require('lazy').setup({
   require 'plugins.undotree',
   require 'plugins.treesitter',
   require 'plugins.filetree',
+  require 'kickstart.plugins.autopairs',
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
