18b5f47fe201a40ed35d2e571f0ccd5a532100e8
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-04-23 13:50:26 +0000

    feat(tailwind): add colors to autocompletion and delete ruby configs

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -36,6 +36,8 @@ return {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-buffer',
+      { 'onsails/lspkind.nvim', opts = {} },
+      { 'roobert/tailwindcss-colorizer-cmp.nvim', opts = {} },
     },
     config = function()
       -- See `:help cmp`
@@ -44,6 +46,28 @@ return {
       luasnip.config.setup {}
 
       cmp.setup {
+        window = {
+          completion = {
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            local kind = require('lspkind').cmp_format {
+              mode = 'symbol_text',
+              maxwidth = 50,
+              before = require('tailwind-tools.cmp').lspkind_format,
+            }(entry, vim_item)
+            local strings = vim.split(kind.kind, '%s', { trimempty = true })
+            kind.kind = ' ' .. (strings[1] or '') .. ' '
+            kind.menu = '    (' .. (strings[2] or '') .. ')'
+
+            return kind
+          end,
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
--- nvim/lua/plugins/tailwind.lua
+++ nvim/lua/plugins/tailwind.lua
@@ -19,14 +19,9 @@ return {
     opts = {},
   },
   {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      { 'roobert/tailwindcss-colorizer-cmp.nvim', opts = {} },
+    'NvChad/nvim-colorizer.lua',
+    opts = {
+      user_default_options = { tailwind = true },
     },
-    opts = function(_, opts)
-      opts.formatting = {
-        format = require('tailwindcss-colorizer-cmp').formatter,
-      }
-    end,
   },
 }
