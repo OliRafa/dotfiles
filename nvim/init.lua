625aa990e6d9a92a1dcd5eb190d92fafef28e721
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-19 11:13:05 +0100

    feat: add editorconfig support

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.editorconfig = true
 
 require 'options'
 require 'remaps'
