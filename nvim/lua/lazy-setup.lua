630e818580ae4653ef6cacaa1db0180945f253eb
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-07-29 18:05:40 +0100

    fix(gitsigns): fix gitsings givin errors in tests plugin by removing its keymaps

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -99,7 +99,6 @@ require('lazy').setup({
 
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.gitsigns',
 
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.lint',
