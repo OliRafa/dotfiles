a1ce5308960d0087b2946d4c3ed17edb64aba4a6
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-05 19:21:24 +0000

    fix(nvim): python path not being correctly loaded for poetry

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -150,21 +150,6 @@ return {
         end,
       })
 
-      -- Disable hover capability from Ruff in favor or Pyright
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client == nil then
-            return
-          end
-          if client.name == 'ruff' then
-            -- Disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = false
-          end
-        end,
-        desc = 'LSP: Disable hover capability from Ruff',
-      })
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
@@ -204,7 +189,9 @@ return {
       local ensure_installed = vim.tbl_keys(opts.servers or {})
       vim.list_extend(ensure_installed, {
         'ansible-lint',
+        'basedpyright',
         'stylua', -- Used to format Lua code
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- dot-config/nvim/lua/plugins/python.lua
+++ dot-config/nvim/lua/plugins/python.lua
@@ -1,34 +1,5 @@
 return {
   {
-    'neovim/nvim-lspconfig',
-    opts = {
-      servers = {
-        ruff = {
-          cmd_env = { RUFF_TRACE = 'messages' },
-          init_options = {
-            settings = {
-              logLevel = 'error',
-            },
-          },
-        },
-        pyright = {
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     'mfussenegger/nvim-dap-python',
     dependencies = {
       'mfussenegger/nvim-dap',
