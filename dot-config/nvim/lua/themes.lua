7f41094520d5f6fe0adb8ee0c581858309cc1dfd
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-16 12:34:47 +0000

    refac(theme): change theme to one that is acceptable for non-truecolors terminals

--- dot-config/nvim/lua/themes.lua
+++ dot-config/nvim/lua/themes.lua
@@ -4,20 +4,10 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'loctvl842/monokai-pro.nvim',
+    'VonHeikemen/little-wonder',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('monokai-pro').setup {
-        styles = {
-          comments = { italic = true },
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'monokai-pro-octagon'
+      vim.cmd.colorscheme 'lw-mariana'
     end,
   },
 }
