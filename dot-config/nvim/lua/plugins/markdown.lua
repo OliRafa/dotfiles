df72f3c12b69e6b7537d4184baf69ed56c922dc3
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-30 11:21:21 +0000

    refac(markdown): style output

--- dot-config/nvim/lua/plugins/markdown.lua
+++ dot-config/nvim/lua/plugins/markdown.lua
@@ -85,13 +85,11 @@ return {
         width = 'block',
         right_pad = 1,
       },
-      heading = {
-        sign = false,
-        icons = {},
-      },
       checkbox = {
-        enabled = false,
+        checked = { scope_highlight = '@markup.strikethrough' },
+        custom = { todo = { rendered = '◯ ' } },
       },
+      sign = { enabled = false },
     },
     ft = { 'markdown', 'norg', 'rmd', 'org', 'codecompanion' },
     config = function(_, opts)
@@ -102,5 +100,6 @@ return {
         set = require('render-markdown').set,
       }):map '<leader>um'
     end,
+    render_modes = { 'n', 'c', 't' },
   },
 }
